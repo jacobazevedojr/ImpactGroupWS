d4df34b2ae9c30e48443351f4250533eaafb33f4
Jacob Azevedo Jr <39184485+jacobazevedojr@users.noreply.github.com>
2020-10-24 10:16:06 -0700

    Revert "Fixing conflicts"
    This reverts commit 999bb2cc4697ba090e0bc9f162d2eb03fee352c3.

---
+++ src/aboutTIG.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function About() {
+  return (
+      <div>
+        <p>This is About</p>
+      </div>
+  );
+}
+
+export default About;
---
+++ src/research.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Research() {
+  return (
+      <div>
+        <p>This is Research</p>
+      </div>
+  );
+}
+
+export default Research;
